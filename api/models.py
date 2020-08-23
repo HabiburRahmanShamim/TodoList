7ec7a468518505603f589589f15c36a3e60eccc4
HabiburRahmanShamim <shamimmahmed04@gmail.com>
2020-08-23 15:16:41 +0600

    Added CRUD API

--- api/models.py
+++ api/models.py
@@ -1,10 +1,13 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
 class Task(models.Model):
   title = models.CharField(max_length=200)
   completed = models.BooleanField(default=False, blank=True, null=True)
+  created_at = models.DateTimeField(default=timezone.now)
+  updated_at = models.DateTimeField(auto_now=True)
       
   def __str__(self):
     return self.title

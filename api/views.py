bbdc9172923c541e8c570a3ac5e13ac8c901de3e
HabiburRahmanShamim <shamimmahmed04@gmail.com>
2020-08-23 17:19:02 +0600

    added CRUD API for task

--- api/views.py
+++ api/views.py
@@ -4,6 +4,8 @@ from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+from .models import Task
+from .serializers import TaskSerializer
 
 # Create your views here.
 
@@ -21,7 +23,7 @@ def apiOverview(request):
 
 @api_view(['GET'])
 def taskList(request):
-	tasks = Task.objects.all().order_by('-id')
+	tasks = Task.objects.all().order_by('created_at')
 	serializer = TaskSerializer(tasks, many=True)
 	return Response(serializer.data)
 

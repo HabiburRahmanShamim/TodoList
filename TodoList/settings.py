dc00428975eec59366a15a4bc6f8125a39e98a34
HabiburRahmanShamim <shamimmahmed04@gmail.com>
2020-08-23 17:59:49 +0600

    added frontend app

--- TodoList/settings.py
+++ TodoList/settings.py
@@ -11,10 +11,13 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates') 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -56,7 +59,7 @@ ROOT_URLCONF = 'TodoList.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
